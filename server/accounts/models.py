7fc209ad6e17c1436a9bb6f51a03e5b388479724
sablindmitry <40712136+sablindmitry@users.noreply.github.com>
2019-06-18 20:46:07 +0400

    Add files via upload

---
+++ server/accounts/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.db import models
+# Create your models here.
+
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+
+    def _str_(self):
+        return "@{}".format(self.username)
+
+
