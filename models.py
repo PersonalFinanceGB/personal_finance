6013e51919b15b71d7d219d40f989c8f88d1b494
sablindmitry <40712136+sablindmitry@users.noreply.github.com>
2019-06-26 13:37:48 +0400

    Толя, это вроде работает

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    name = AbstractUser.username  
+    #name = models.CharField(blank=False, max_length=255, U)
+    def __str__(self):
+        return self.email
