218e3d8aabffe940425e3b916761de67919025ad
sablindmitry <40712136+sablindmitry@users.noreply.github.com>
2019-06-26 13:40:47 +0400

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    name = AbstractUser.username  
-    #name = models.CharField(blank=False, max_length=255, U)
-    def __str__(self):
-        return self.email
