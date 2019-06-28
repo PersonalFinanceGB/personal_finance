7b6a1e36f28754ff0abe1fba4f7bfbde45e726d3
ShishkinAV <shishkinanatoli@gmail.com>
2019-06-28 16:08:38 +0400

    неудачные попытке внедрения дополнительного контекста к возвращаемым в API

--- server/acct/serializers.py
+++ server/acct/serializers.py
@@ -1,5 +1,5 @@
 from .models import User_acct
-from rest_framework import serializers
+from rest_framework import serializers, request
 
 
 class BillSerializer(serializers.ModelSerializer):
@@ -10,3 +10,8 @@ class BillSerializer(serializers.ModelSerializer):
         model = User_acct
         fields = ('id_user', 'id_acct', 'sum')
 
+    # def to_representation(self, instance):
+    #     ret = super().to_representation(instance)
+    #     # Access self.context here to add contextual data into ret
+    #     ret['test'] = self.context['test']
+    #     return ret
--- server/acct/views.py
+++ server/acct/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, request
 from acct.models import User_acct
 from acct.serializers import BillSerializer
 
@@ -12,3 +12,12 @@ class BillView(generics.ListAPIView):
     queryset = User_acct.objects.all()
     serializer_class = BillSerializer
 
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context()
+    #     context['test'] = 'test'
+    #     return context
+    #
+    # def get_renderer_context(self):
+    #     context = super().get_renderer_context()
+    #     context['foo'] = 'bar'
+    #     return context
