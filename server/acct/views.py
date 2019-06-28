874529558aa634a0989c38963394634b88ad67fe
ShishkinAV <shishkinanatoli@gmail.com>
2019-06-28 13:36:51 +0400

    add queryset for Bill

--- server/acct/views.py
+++ server/acct/views.py
@@ -9,4 +9,6 @@ class BillView(generics.ListAPIView):
     Returns a list of all authors.
     """
     model = User_acct
+    queryset = User_acct.objects.all()
     serializer_class = BillSerializer
+
